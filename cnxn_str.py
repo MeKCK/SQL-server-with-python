8ac05ecd3d0506b9570ad734b2f7d32d62c4f846
kiprono <42340517+MeKCK@users.noreply.github.com>
2020-05-28 22:07:33 +0300

    first commit

---
+++ cnxn_str.py
@@ -0,0 +1,17 @@
+# accessing db over a trusted network 
+ 
+cnxn_str = ("Driver={SQL Server Native Client 11.0};"
+            "Server=UKXXX00123, 45600;"
+            "Database=DB01;"
+            "Trusted_Connection=yes;")
+
+# accessing over non-secure network login (pass and username included)
+cnxn_str = ("Driver={SQL Server Native Client 11.0};"
+            "Server=UKXXX00123,45600;"
+            "Database=DB01;"
+            "UID=JoeBidden;"
+            "PWD=Password123;")
+
+#initialize connection 
+cnxn = pyodbc.connect (cnxn_str)
+
